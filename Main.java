ccdd509a51e539aee8630694ccc787bb78c181a0
AKhald <73669714+Kanisse@users.noreply.github.com>
2024-12-03 11:50:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-public class Main {
-  public void afficher(){
-    System.out.print("Hello world!!");
-  }
-}
+
 
 public static void main (){
-Main m = new Main();
-  m.afficher();
+System.out.print("Hello world!!");
 }
